976c640962a74059816df310e3b1db655bdc2a7b
Irang <irang0426@gmail.com>
2022-12-21 22:20:43 +0900

    feat: programmers120894

---
+++ src/irang/programmers/number120894/Solution.java
@@ -0,0 +1,22 @@
+package irang.programmers.number120894;
+
+public class Solution {
+    public static long solution(String numbers) {
+        return Long.parseLong(numbers.replaceAll("zero", "0")
+                .replaceAll("one", "1")
+                .replaceAll("two", "2")
+                .replaceAll("three", "3")
+                .replaceAll("four", "4")
+                .replaceAll("five", "5")
+                .replaceAll("six", "6")
+                .replaceAll("seven", "7")
+                .replaceAll("eight", "8")
+                .replaceAll("nine", "9")
+        );
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("onetwothreefourfivesixseveneightnine"));
+        System.out.println(solution("onefourzerosixseven"));
+    }
+}
