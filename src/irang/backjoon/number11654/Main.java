a9675cc51775a666945a54dddfae07e3734fb12c
irang <irang0426@gmail.com>
2023-05-24 00:48:45 +0900

    feat: backjoon11654

---
+++ src/irang/backjoon/number11654/Main.java
@@ -0,0 +1,15 @@
+package irang.backjoon.number11654;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char ch = br.readLine().charAt(0);
+        int result = ch;
+        System.out.println(result);
+        br.close();
+    }
+}
